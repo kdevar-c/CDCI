f4e9dcec123f610f6182b78ccb4aeecd3094256f
kdevarakonda1975 <kamalakar.d@gmail.com>
2017-07-25 16:56:18 -0500

    Modified the code
    Modified the code using Java script executor and added the assert statements

--- YelpTest/src/test/java/com/yelptest/AppTest.java
+++ YelpTest/src/test/java/com/yelptest/AppTest.java
@@ -1,19 +1,31 @@
 package com.yelptest;
 
 import java.util.List;
+import java.util.concurrent.*;
+
+import javax.swing.text.Document;
 
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.SystemConfiguration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.relevantcodes.extentreports.DisplayOrder;
@@ -33,27 +45,72 @@ public class AppTest {
 			System.out.println("Could not load config.properties file. Please fix and continue");
 		}
 	}
-
+	
+	@BeforeTest
+    @Parameters("browser")
+    public void setup(String browserName) throws Exception{
+        if (browserName.equalsIgnoreCase("Firefox")) {
+			System.setProperty("webdriver.gecko.driver", config.getString("webdriver.firefox", null));
+			//System.setProperty("webdriver.firefox.bin", config.getString("webdriver.firefoxbin", null));
+            driver = new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			
+        }
+        else if (browserName.equalsIgnoreCase("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", config.getString("webdriver.chrome", null));
+            driver = new ChromeDriver();
+        }
+        else if (browserName.equalsIgnoreCase("ie")) {
+            System.setProperty("webdriver.edge.driver", config.getString("webdriver.ie", null));
+            driver = new EdgeDriver();
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			
+        }
+		 else if (browserName.equalsIgnoreCase("Safari")) {
+            //System.setProperty("webdriver.safari.driver", config.getString("webdriver.safari", null));
+            driver = new SafariDriver();
+        }
+        else {
+            throw new Exception("Browser is not correct");
+        }
+    }
+	
 	CompositeConfiguration config;
 	// Declare Web Driver variables
-	ChromeDriver driver;
+	WebDriver driver;
 	// Declare Extent Reports
 	ExtentReports reports;
 
 	ExtentTest test = null;
-
+	
 	@Test(dataProvider = "dp")
-	public void Yelp_Test(String sPriceRange, String sFeature) {
+	public void yelpTest(String sPriceRange, String sFeature) throws Exception{
 		// precondition is to download the driver
 		// read driver path from property config.
-		System.setProperty("webdriver.chrome.driver", config.getString("webdriver.chrome", null));
-		// Create new object of chrome webdriver.
-		driver = new ChromeDriver();
-
 		// Test 1. Below step is open the yelp page.
-		driver.get("http://www.yelp.com");
-		// Maximize the window
-		driver.manage().window().maximize();
+		
+				driver.get("http://www.yelp.com");
+				
+				// Maximize the window
+				driver.manage().window().maximize();
+				
+		// Kamal: 07/22 
+		//Java script code
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		String sURL= js.executeScript("return document.title").toString();
+		System.out.println(sURL);
+		//Assert.assertEquals("Dallas Restaurants, Dentists, Bars, Beauty Salons, Doctors - Yelp", sURL);
+		
+		//js.executeScript("window.location-'http://www.yelp.com");
+		// entering text in find as Restaurants
+		js.executeScript("document.getElementById('find_desc').value='Restaurants'");
+		// click the find button
+		js.executeScript("document.getElementById('header-search-submit').click()");
+		// entering the restaurants-pizza in search.		
+		js.executeScript("document.getElementById('find_desc').value='Restaurants - Pizza'");
+		// Click submit
+		js.executeScript("document.getElementById('header-search-submit').click()");
+		
 		// Initiate Extent Reports
 		reports = new ExtentReports(config.getString("reports.extent.html", null), false, DisplayOrder.NEWEST_FIRST);
 		// Declare Start test name
@@ -61,29 +118,30 @@ public class AppTest {
 		test.log(LogStatus.PASS, "Test Case 1: Browser is opened and window is Maximized and Yelp home page is displayed.");
 		
 		// Test 2. Below step clicks Restaurants on yelp home page.
-		driver.findElement(By.linkText("Restaurants")).click();
+		//driver.findElement(By.linkText("Restaurants")).click();
 		test.log(LogStatus.PASS, "Test Case 2: User clicks Restaurants on yelp home page");
 		// Using the wait to load the page with Information
 		WebDriverWait wait = new WebDriverWait(driver, 60);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Restaurants")));
 		
 		// Test 3. Click Search button
-		driver.findElement(By.linkText("Restaurants")).click();
+		//driver.findElement(By.linkText("Restaurants")).click();
 		test.log(LogStatus.PASS, "Test Case 3 : User clicks search button");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[id='find_desc']")));
 		// The below state will clear the previously entered text "Restaurants"
+		//js.executeScript("document.querySelector(\"input[id='find_desc']\").clear()");
 		driver.findElement(By.cssSelector("input[id='find_desc']")).clear();
 		
 		// Test 4. Appending the Pizza Restaurants to search
 		test.log(LogStatus.PASS, "Test Case 4 : Appending the Pizza Restaurants to search");
-		driver.findElement(By.cssSelector("input[id='find_desc']")).sendKeys("Restaurants Pizza"); // Restaurants Pizza
-		driver.findElement(By.cssSelector("button[id='header-search-submit']")).click();
-
+		//driver.findElement(By.cssSelector("input[id='find_desc']")).sendKeys("Restaurants Pizza"); // Restaurants Pizza
+		//driver.findElement(By.cssSelector("button[id='header-search-submit']")).click();
+		Thread.sleep(1000);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[style='display: inline;']")));
 		String beforeloadingpage = driver.findElement(By.className("pagination-results-window")).getText();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pagination-results-window")));
 		String SearchResultsCount = driver.findElement(By.className("pagination-results-window")).getText().split(" ")[3];
-		
+		Assert.assertNotEquals(beforeloadingpage, SearchResultsCount);
 		// Test 5: Below print state will print total no.of Search results with
 		// no.of results in the current page
 		if (beforeloadingpage != SearchResultsCount) {
@@ -92,7 +150,7 @@ public class AppTest {
 			test.log(LogStatus.FAIL, "total no.of Search results with no.of results in the current page are not equal");
 		}
 		System.out.println("Total no. of Search results: " + SearchResultsCount);
-		// Assert.assertEquals("2542", SearchResultsCount);
+		//Assert.assertEquals("2625", SearchResultsCount);
 
 		Reporter.log("Total no. of Search results: " + SearchResultsCount);
 
@@ -102,26 +160,29 @@ public class AppTest {
 		String sAddress = "5702 Richmond Ave, Dallas, TX 75206";
 
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			System.out.println("InterruptedException - during thread sleep");
 			e.printStackTrace();
 		}
-		driver.findElement(By.className("container"))
-				.findElement(By.cssSelector("input[value='RestaurantsPriceRange2." + sPriceRange + "']")).click();
-		;
+		
+		//driver.findElement(By.className("container"))
+			//	.findElement(By.cssSelector("input[value='RestaurantsPriceRange2." + sPriceRange + "']")).click();
+		js.executeScript("document.querySelector(\"input[value='RestaurantsPriceRange2.1']\").click()");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[style='display: inline;']")));
 		
 		// Test 6. clicking the filters and applying the filters
-		String[][] ListOfFeatures = { { "Order Delivery", "PlatformDelivery" }, { "Order Pickup", "PlatformPickup" },
-				{ "Open Now", "open_now" }, { "Take-out", "RestaurantsTakeOut" } };
-		for (int i = 0; i < 4; i++)
-			if (ListOfFeatures[i][0].equalsIgnoreCase(sFeature)) {
-				driver.findElement(By.className("container"))
-						.findElement(By.cssSelector("input[value='" + ListOfFeatures[i][1] + "']")).click();
-				break;
-			}
+		//String[][] ListOfFeatures = { { "Order Delivery", "PlatformDelivery" }, { "Order Pickup", "PlatformPickup" },
+			//	{ "Open Now", "open_now" }, { "Take-out", "RestaurantsTakeOut" } };
+		if ("Order Delivery".equalsIgnoreCase(sFeature)) {
+			js.executeScript("document.querySelector(\"input[value='PlatformDelivery']\").click()");
+		}
+		if ("Order Pickup".equalsIgnoreCase(sFeature)) {
+			js.executeScript("document.querySelector(\"input[value='PlatformPickup']\").click()");
+		}
+			
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[style='display: inline;']")));
+		
 		if (sFeature.equalsIgnoreCase("Order Delivery")) {
 			driver.findElement(By.className("address-picker")).sendKeys(sAddress);
 			// icon--18-search-small
@@ -145,13 +206,16 @@ public class AppTest {
 		eSelectRestaurant.findElement(By.className("biz-name")).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("biz-page-header")));
 		
-		// Test 11. Log all critical information of the selected restaurant
+		// Test 11. Log all critical information of the selected restauran
 		// details,for the reporting purpose . Address, Phone No, web site
 		// details . First 3 customer reviews
+		Thread.sleep(2000);
 		System.out.println("****************************************************************************************");
 		System.out.println("Restaurant Name: " + driver.findElement(By.className("biz-page-title")).getText());
 		System.out.println("Address: " + driver.findElement(By.tagName("address")).getText());
 		System.out.println("Phone Number: " + driver.findElement(By.className("biz-phone")).getText());
+		
+		//System.out.println("Website: " + driver.findElement(By.className("biz-website")).getText());
 		System.out.println("Website: " + driver.findElement(By.className("biz-website")).getText());
 		Reporter.log("****************************************************************************************");
 		Reporter.log("Restaurant Name: " + driver.findElement(By.className("biz-page-title")).getText());
@@ -175,8 +239,8 @@ public class AppTest {
 
 	}
 
-	@AfterMethod
-	public void afterMethod() {
+	@AfterTest
+	public void afterTest() {
 		// Below method will close the browser
 		driver.quit();
 		reports.flush();
